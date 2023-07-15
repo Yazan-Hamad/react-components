67b316990acd3f72b4d264e2bf69339083590a5c
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-15 23:07:30 +0300

    render cells dynamically

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,16 +1,28 @@
-import Table from '../components/Table';
+import Table from "../components/Table";
 
 function TablePage() {
   const data = [
-    { name: 'Orange', color: 'bg-orange-500', score: 5 },
-    { name: 'Apple', color: 'bg-red-500', score: 3 },
-    { name: 'Banana', color: 'bg-yellow-500', score: 1 },
-    { name: 'Lime', color: 'bg-green-500', score: 4 },
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Banana", color: "bg-yellow-500", score: 1 },
+    { name: "Lime", color: "bg-green-500", score: 4 },
   ];
+  const config = [
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+    },
+    { label: "Color", render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`}></div> },
+    { label: "Score", render: (fruit) => fruit.score},
+  ];
+
+  const keyFn = (fruit)=>{
+    return fruit.name;
+  }
 
   return (
     <div>
-      <Table data={data} />
+      <Table config={config} data={data} keyFn={keyFn} />
     </div>
   );
 }
