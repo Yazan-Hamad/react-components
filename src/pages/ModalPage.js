4e437fc9254de525e13f647da34000934e134856
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-15 17:39:50 +0300

    customize modal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -4,10 +4,24 @@ import Modal from "../components/Modal";
 
 function ModalPage(){
    const [showModal, setShowModal] = useState(false);
-
+   const modalContent = <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   </p>;
+   const modalButton = <Button 
+   primary
+   onClick={()=>{setShowModal(false)}}
+   >
+    Accept
+    </Button>;
+    const modal = <Modal 
+    onClose={()=>{setShowModal(false)}}
+    actionBar={modalButton}
+    >
+{modalContent}
+    </Modal>
     return <div>
         <Button primary onClick={()=>{setShowModal(true)}}>Open Modal</Button>
-        { showModal && <Modal onClose={()=>{setShowModal(false)}}/>}
+        { showModal && modal}
     </div>;
 }
 
