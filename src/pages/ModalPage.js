d238e4807607b7a18553cbe782cbeafd37ed75ba
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-15 17:27:20 +0300

    create portal for modal

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -4,10 +4,11 @@ import Modal from "../components/Modal";
 
 function ModalPage(){
    const [showModal, setShowModal] = useState(false);
+
     return <div>
         <Button primary onClick={()=>{setShowModal(true)}}>Open Modal</Button>
-        { showModal && <Modal/>}
-    </div>
+        { showModal && <Modal onClose={()=>{setShowModal(false)}}/>}
+    </div>;
 }
 
 export default ModalPage;
