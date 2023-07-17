6dbff336895d55b719ba487ca9bd94232714ea7a
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-17 14:04:51 +0300

    refactor useState to useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,30 +1,78 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
 
+const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
+const SET_ADDED_VALUE = "change-added-value";
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.addedValue,
+        addedValue: 0,
+      };
+    case SET_ADDED_VALUE:
+      return {
+        ...state,
+        addedValue: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
 function CounterPage({ initialCount }) {
-  const [count, setCount] = useState(initialCount);
-  const [addedValue, setAddedValue] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    addedValue: 0,
+  });
 
   const increment = () => {
-    setCount(count + 1);
+    dispatch({
+      type:"increment"
+    });
   };
+
   const decrement = () => {
-    setCount(count - 1);
+    dispatch({
+      type:"decrement"
+    });
   };
 
   const onChangeHandler = (event) =>{
-    setAddedValue( parseInt(event.target.value) || 0 );
-  }
+
+    dispatch({
+      type: SET_ADDED_VALUE,
+      payload: parseInt(event.target.value) || 0
+    })
+  };
+
   const onSubmitHandler = (event) =>{
     event.preventDefault();
-    setCount( count + addedValue);
-    setAddedValue(0);
-  }
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    });
+  };
 
   return (
     <Panel className="m-3">
-      <h1 className="text-lg">Count is {count}</h1>
+      <h1 className="text-lg">Count is {state.count}</h1>
       <div className="flex flex-row">
         <Button onClick={increment}>Increment</Button>
         <Button onClick={decrement}>Decrement</Button>
@@ -33,7 +81,7 @@ function CounterPage({ initialCount }) {
       <form onSubmit={onSubmitHandler}>
         <label>Add a lot!</label>
         <input
-        value={addedValue || ""}
+        value={state.addedValue || ""}
         onChange={onChangeHandler}
           type="number"
           className="p-1 m-3 bg-gray-50 border border-gray-300"
